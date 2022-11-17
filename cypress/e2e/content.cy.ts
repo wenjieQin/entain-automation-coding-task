99132544ffbe28c1d3d5251ab3c57ff5e7879018
wenjieQin <qin900813@outlook.com>
2022-11-18 09:02:40 +1100

    toggle check boxes  and test category id
    add toggle checkbox

--- cypress/e2e/content.cy.ts
+++ cypress/e2e/content.cy.ts
@@ -1,4 +1,6 @@
 import { RACING_CATEGORIES } from "../config/constants";
+import { CategoryFilters } from "../../src/components/CategoryFilters/CategoryFilters";
+import { NextToGo } from '../../src/components/NextToGo';
 
 describe('Page Content', () => {
   it('Should correctly display page title', () => {
@@ -19,69 +21,86 @@ describe('Page Content', () => {
         });
       })
     });
-  })
+  });
 
+  //uncheck any checkbox 
+  //get the category id of the check box 
+  //any id attribute (=race.categoryid) in next to go list shouldn't contain this category id
+  it('Toggle checkboxes and it should show only selected categories', () => {
+      cy.get('[data-testid=category-filters]').then(() => {
+        RACING_CATEGORIES.forEach((category) => {
+          cy.get(`[data-testid=category-filter-${category.categoryId}]`).then(() => {
+              cy.get('[data-testid=category-filter-checkbox]').uncheck();
+              console.log(cy.get('[data-testid="Next2go"]').invoke('attr', 'id'));
+              cy.get('[data-testid="Next2go"]').invoke('attr', 'id').should('not.contain', category.categoryId)           
+          });
+        })
+      });
 
-  it('Should have 5 items in the list', () => {
-    cy.visit('');
-    cy.get('[data-testid="Next2go"]').should(($lis) => {
-      expect($lis).to.have.length(5);
     });
-  })
-
-  it('Should have content for race name/number and countdown', () => {
-    cy.visit('');
-    for (let i = 0; i < 5; i++) {
-      cy.get('[data-testid="next2go-list"]>div>div>b').eq(i).should('not.be.empty'); //race number
-      cy.get('[data-testid="next2go-list"]>div>div>p').eq(i).should('not.be.empty'); //race name
-      cy.get('[data-testid="next2go-list"]>div>p').eq(i).should('not.be.empty'); //count down timer
-      //  cy.get(':nth-child(2) > .race-name > p').should('not.be.empty');
-      //  cy.get(':nth-child(2) > .race-name > .race-number').should('not.be.empty');
-      //  cy.get('[data-testid="LoadingSpinner"] > :nth-child(1) > :nth-child(1) > :nth-child(2)').should('not.be.empty');
-    }
-  })
-
-
-  it('if countdown less than -59s, the race should be removed', () => {
-    cy.visit('');
-    for (let i = 0; i < 5; i++) {
-      cy.get('[data-testid="count-down-timer"]').eq(i).then((time) => {
-        if (time.text().includes('m')) {
-          //time.invoke('text').should('not.have.text', 'm')
-          cy.wrap(time).should('not.contain', '-')
-        }
-      })
-    }
-  })
 
 
 
-  it('should update the count down timer string every second', () => {
-    cy.visit('');
+it('Should have 5 items in the list', () => {
+  cy.visit('');
+  cy.get('[data-testid="Next2go"]').should(($lis) => {
+    expect($lis).to.have.length(5);
+  });
+})
 
-    for (let i = 0; i < 5; i++) {
-      //found that if countdown greater than 5m, will not display countdown for seconds
-      cy.get('[data-testid="count-down-timer"]').eq(i).then((time) => {
-        if (time.text().includes('s')) {
-          cy.get('[data-testid="count-down-timer"]').eq(i).invoke('text').
-            then((timmer1) => {
-              cy.wait(2000); //set time interval 2s
-              cy.get('[data-testid="count-down-timer"]').eq(i).invoke('text').should((timmer2) => { expect(timmer1).not.to.equal(timmer2) })
-            }
-            )
-        }
-        else {  //if not contians 's', means greater than 5m
-          cy.get('[data-testid="count-down-timer"]').eq(i).invoke('text').
-            then((timmer1) => {
-              cy.wait(60000); //set time interval 1min
-              cy.get('[data-testid="count-down-timer"]').eq(i).invoke('text').should((timmer2) => { expect(timmer1).not.to.equal(timmer2) })
-            }
-            )
-        }
-      })
-    }
+it('Should have content for race name/number and countdown', () => {
+  cy.visit('');
+  for (let i = 0; i < 5; i++) {
+    cy.get('[data-testid="next2go-list"]>div>div>b').eq(i).should('not.be.empty'); //race number
+    cy.get('[data-testid="next2go-list"]>div>div>p').eq(i).should('not.be.empty'); //race name
+    cy.get('[data-testid="next2go-list"]>div>p').eq(i).should('not.be.empty'); //count down timer
+    //  cy.get(':nth-child(2) > .race-name > p').should('not.be.empty');
+    //  cy.get(':nth-child(2) > .race-name > .race-number').should('not.be.empty');
+    //  cy.get('[data-testid="LoadingSpinner"] > :nth-child(1) > :nth-child(1) > :nth-child(2)').should('not.be.empty');
   }
-  );
+})
+
+
+it('if countdown less than -59s, the race should be removed', () => {
+  cy.visit('');
+  for (let i = 0; i < 5; i++) {
+    cy.get('[data-testid="count-down-timer"]').eq(i).then((time) => {
+      if (time.text().includes('m')) {
+        //time.invoke('text').should('not.have.text', 'm')
+        cy.wrap(time).should('not.contain', '-')
+      }
+    })
+  }
+})
+
+
+
+it('should update the count down timer string every second', () => {
+  cy.visit('');
+
+  for (let i = 0; i < 5; i++) {
+    //found that if countdown greater than 5m, will not display countdown for seconds
+    cy.get('[data-testid="count-down-timer"]').eq(i).then((time) => {
+      if (time.text().includes('s')) {
+        cy.get('[data-testid="count-down-timer"]').eq(i).invoke('text').
+          then((timmer1) => {
+            cy.wait(2000); //set time interval 2s
+            cy.get('[data-testid="count-down-timer"]').eq(i).invoke('text').should((timmer2) => { expect(timmer1).not.to.equal(timmer2) })
+          }
+          )
+      }
+      else {  //if not contians 's', means greater than 5m
+        cy.get('[data-testid="count-down-timer"]').eq(i).invoke('text').
+          then((timmer1) => {
+            cy.wait(60000); //set time interval 1min
+            cy.get('[data-testid="count-down-timer"]').eq(i).invoke('text').should((timmer2) => { expect(timmer1).not.to.equal(timmer2) })
+          }
+          )
+      }
+    })
+  }
+}
+);
 
 
 });
